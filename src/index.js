17499a74cb3a1b1fe4f05763e607818ccf287f31
github username <github email address>
2019-07-16 12:13:45 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,61 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = "https://dog.ceo/api/breeds/list/all"
+const dropDown = document.getElementById('breed-dropdown')
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetchDogs()
+    fetchBreeds()
+  })
+
+function fetchDogs() {
+    return fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(json => renderImages(json.message));
+  }
+
+  function fetchBreeds() {
+    return fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(json => breedList(json.message));
+  }
+
+  function breedList(data) {
+    const dogList = document.getElementById('dog-breeds')
+    Object.keys(data).forEach(breed => {
+          dogList.innerHTML += `<li onclick="changeColor(this)">${breed} </li>` ;
+        })
+    }
+
+function renderImages(data) {
+    const dogBox = document.getElementById('dog-image-container')
+      data.forEach(dog => {
+        const img = document.createElement('img')
+        img.src = `${dog}`
+        dogBox.appendChild(img)
+    })
+}
+
+function changeColor(element) {
+    element.style.color = "blue";
+}
+
+function filterBreeds(element) {
+    let value = element.value;  
+    let listElements = document.querySelectorAll('li')
+    listElements.forEach(breed=>{
+        if (breed.innerText[0] != value){
+            // console.log(breed.innerText)
+             breed.style.display = "none";
+        } else {
+            breed.style.display = "";
+        }
+    })
+}
+
+
+
+
+
+
+  
